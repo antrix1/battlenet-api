b9b25a51c6dccf66863445ac61cfc8b63374fd09
Andrej Dragojevic <andrejdr.93@gmail.com>
2020-09-18 03:56:50 +0200

    Add namespace to pvp leaderboard

--- lib/battlenet/modules/wow/pvp_leaderboard.rb
+++ lib/battlenet/modules/wow/pvp_leaderboard.rb
@@ -2,15 +2,16 @@ module Battlenet
   module WOW
     class PVPLeaderboard < Battlenet::APIResponse
       def initialize(options={})
-        @bracket        = options.delete(:bracket)
-        @season         = options.delete(:season)
-        @endpoint       = "/data/wow/pvp-season/#{@season}/pvp-leaderboard/#{@bracket}"
+        @bracket  = options.delete(:bracket)
+        @season   = options.delete(:season)
+        @endpoint = "/data/wow/pvp-season/#{@season}/pvp-leaderboard/#{@bracket}"
+        @region   = options[:client].region
 
         super(options)
       end
 
-      def details
-        get_data(@endpoint)
+      def details(options = {})
+        get_data(@endpoint, options.merge(namespace: "dynamic-#{@region}"))
       end
     end
   end
